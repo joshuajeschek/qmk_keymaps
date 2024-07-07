9aa5fbfdf52a8406ff86e4ce3b47b57a4dc8ff56
Joshua Jeschek <dev@jeschek.eu>
2024-07-07 10:59:26 +0200

    feat(gaming): modify toggle key

--- indil/keymap.c
+++ indil/keymap.c
@@ -40,11 +40,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // LETTERS AND STUFF
 	[_DEFAULT] = LAYOUT(
-    KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                        KC_6,       KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                        KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
-    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,    KC_G,                        KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,    KC_B,       _x_,    KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, DYNENT,  MO(_RAISE), KC_BSPC, KC_RGUI),
+    KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,                            KC_6,       KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
+    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                            KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
+    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,    KC_G,                            KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,    KC_B,       KC_MPLY,    KC_MPLY, KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, TG(_GAMING),
+                         KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,     DYNENT,  MO(_RAISE), KC_BSPC, KC_RGUI),
   // SYMBOLS
 	[_LOWER] = LAYOUT(
     ___,     KC_F1,      KC_F2,   KC_F3,      KC_F4,   KC_F5,                KC_F6,         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
@@ -61,17 +61,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           ___,    ___,    MO(_ADJUST),  ___, ___, ___,     ___,     ___),
   // KEYBOARD SPECIFIC STUFF I GUESS
 	[_ADJUST] = LAYOUT(
-    _x_, _x_, _x_, _x_, _x_, _x_,                   _x_, _x_, _x_,     _x_,     _x_,     _x_,
-    _x_, _x_, _x_, _x_, _x_, _x_,                   _x_, _x_, _x_,     _x_,     _x_,     _x_,
-    _x_, _x_, _x_, _x_, _x_, TG(_GAMING),           _x_, _x_, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
-    _x_, _x_, _x_, _x_, _x_, _x_,         _x_, _x_, _x_, _x_, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
-                   ___, ___, ___,         ___, ___, ___, ___, ___),
+    _x_, _x_, _x_, _x_, _x_, _x_,           _x_, _x_, _x_,     _x_,     _x_,     _x_,
+    _x_, _x_, _x_, _x_, _x_, _x_,           _x_, _x_, _x_,     _x_,     _x_,     _x_,
+    _x_, _x_, _x_, _x_, _x_, _x_,           _x_, _x_, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+    _x_, _x_, _x_, _x_, _x_, _x_, _x_, _x_, _x_, _x_, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
+                   ___, ___, ___, ___, ___, ___, ___, ___),
   [_GAMING] = LAYOUT(
     KC_ESC, KC_1,    KC_2, KC_3,    KC_4,          KC_5,                    KC_6,  KC_7,    KC_8,    KC_9,   KC_0,    KC_F18,
     KC_F13, KC_TAB,  KC_Q, KC_W,    KC_E,          KC_R,                    KC_T,  KC_Y,    KC_U,    KC_I,   KC_O,    KC_F19,
     KC_F14, KC_LSFT, KC_A, KC_S,    KC_D,          KC_F,                    KC_G,  KC_H,    KC_J,    KC_K,   KC_L,    KC_F20,
-    KC_F15, KC_LCTL, KC_Z, KC_X,    KC_C,          KC_V,    KC_F16, KC_F17, KC_B,  KC_N,    KC_COMM, KC_DOT, KC_SLSH, KC_F21,
-                           KC_LGUI, LALT(KC_TAB),  KC_TAB,  KC_SPC, DYNENT, TG(_GAMING), KC_BSPC, KC_RGUI),
+    KC_F15, KC_LCTL, KC_Z, KC_X,    KC_C,          KC_V,    KC_F16, KC_F17, KC_B,  KC_N,    KC_COMM, KC_DOT, KC_SLSH, TG(_GAMING),
+                           KC_LGUI, LALT(KC_TAB),  KC_TAB,  KC_SPC, DYNENT, MO(_RAISE), KC_BSPC, KC_RGUI),
 };
 
 int dyn_ent_return_to = _DEFAULT;
